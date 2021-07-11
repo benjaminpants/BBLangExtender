c58ebaa1cd22a170a65f230b8fa807697dd7d94d
benjaminpants <maltbyfamily@gmail.com>
2021-07-10 19:00:34 -0700

    Initial commit

---
+++ BBLangExtender/BasePlugin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Net;
+using System.IO;
+//BepInEx stuff
+using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using HarmonyLib;
+using BepInEx.Configuration;
+using System.Collections.Generic;
+
+namespace BBLangExtender
+{
+    [BepInPlugin("mtm101.rulerp.bbplus.bble", "BB+ Language Extender", "1.0.0.0")]
+
+    public class BaldiLanguageExtender : BaseUnityPlugin
+    {
+
+        
+        void Awake()
+        {
+            Harmony harmony = new Harmony("mtm101.rulerp.bbplus.bble");
+            
+            harmony.PatchAll();
+        }
+    }
+}
